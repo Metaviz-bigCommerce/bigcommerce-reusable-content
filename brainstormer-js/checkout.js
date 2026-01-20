7bc0089b1e9f2c4d9983ca7a126526e1e2bbc8f6
H5SH <tipuasad2001@gmail.com>
2026-01-20 06:32:21 -0600

    feat:checkout-mutation

--- brainstormer-js/checkout.js
+++ brainstormer-js/checkout.js
@@ -1,3 +1,35 @@
-<script>
-    console.log('customjs');
-</script>
+function layoutLoaded(node) {
+    const layoutCart = node.querySelector('.layout-cart') || document.querySelector('.cartDrawer');
+    const observer = new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+            if (mutation.type === 'childList' && mutation.addedNodes.length) {
+                console.log(mutation.addedNodes[0], 'node')
+            }
+        }
+    })
+
+    observer.observe(layoutCart, {
+        childList: true,
+        subtree: false,
+    })
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const checkout = document.querySelector('#checkout-app')
+
+    const observer = new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+            if (mutation.type === 'childList') {
+                layoutLoaded(mutation.addedNodes[0])
+            }
+        }
+    })
+
+    observer.observe(checkout, {
+        childList: true,
+        subtree: false,
+    })
+})
